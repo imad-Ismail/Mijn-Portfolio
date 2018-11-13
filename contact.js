4ee890c2541a84864d4b00923388de151f65ca09
Imad Ismail <medoo7911@hotmail.com>
2018-11-13 12:43:21 +0100

    vervijderd

---
+++ contact.js
@@ -0,0 +1,14 @@
+var i = 0;
+var txt = 'Imad Ismail';
+var speed = 100;
+document.getElementById("medo").addEventListener("onmouseover", schrijf());
+
+function schrijf() {
+  if (i < txt.length) {
+    document.getElementById("medo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(schrijf, speed);
+  };
+};
+schrijf();
+
